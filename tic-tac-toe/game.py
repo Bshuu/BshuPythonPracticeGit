d8c165502ca2ebad11983da7d61b938270ed48cc
bryces413 <bryces413@gmail.com>
2023-08-09 09:50:19 -0400

    added more to the board

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -11,4 +11,6 @@ class TicTacToe:
     @staticmethod
     def print_board_nums():
         # 0 | 1 | 2 etc...
-        number_board = 
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)]for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |' )
