e2ddd6517e67a5c03bf0a6353e0be10dfb206f6e
bryces413 <bryces413@gmail.com>
2023-08-09 13:35:41 -0400

    more code

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -14,3 +14,6 @@ class TicTacToe:
         number_board = [[str(i) for i in range(j*3, (j+1)*3)]for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |' )
+
+    def available_moves(self):
+        return
