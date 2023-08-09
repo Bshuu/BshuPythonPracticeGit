3e3b10a13106fe130dab4d500005eddfe4ba8372
Bshuu <bryces413@gmail.com>
2023-08-08 20:51:57 -0400

    update

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -1,4 +1,8 @@
 class TicTacToe:
     def __init__(self):
-        self.board = [' ' for _ in range(9)]
-        
+        self.board = [' ' for _ in range(9)] # using a single list to rep a 3x3 board
+        self.current.winner = None # keep track of winner
+
+    def print_board(self):
+        #this is just getting the rows
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
