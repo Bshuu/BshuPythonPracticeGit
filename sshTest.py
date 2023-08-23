a37984da053ab2ecfd97bafe2d1f42f5b508b505
Bshuu <bryces413@gmail.com>
2023-08-22 20:37:01 -0400

    update ssh

--- sshTest.py
+++ sshTest.py
@@ -1,10 +1,45 @@
-import paramiko
+from paramiko import SSHClient, AutoAddPolicy
+from rich import print, pretty, inspect
+pretty.install()
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('172.18.0.1', username='brshuart',password='')
+client = SSHClient()
+#LOAD HOST KEYS
+#client.load_host_keys('~/.ssh/known_hosts')
+client.load_host_keys('C:/Users/brad/.ssh/known_hosts')
+client.load_system_host_keys()
 
-stdin, stdout, stderr = ssh.exec_command('pwd')
-print(stdout.readlines())
+#Known_host policy
+client.set_missing_host_key_policy(AutoAddPolicy())
 
-ssh.close()
+
+#client.connect('10.1.1.92', username='root', password='password1')
+client.connect('10.1.1.83', username='root')
+
+
+#Run a command (execute PHP interpreter)
+#client.exec_command('hostname')
+stdin, stdout, stderr = client.exec_command('hostname')
+print(type(stdin))
+print(type(stdout))
+print(type(stderr))
+
+# Optionally, send data via STDIN, and shutdown when done
+stdin.write('Hello world')
+stdin.channel.shutdown_write()
+
+# Print output of command. Will wait for command to finish.
+print(f'STDOUT: {stdout.read().decode("utf8")}')
+print(f'STDERR: {stderr.read().decode("utf8")}')
+
+# Get return code from command (0 is default for success)
+print(f'Return code: {stdout.channel.recv_exit_status()}')
+
+# Because they are file objects, they need to be closed
+stdin.close()
+stdout.close()
+stderr.close()
+
+# Close the client itself
+client.close()
+
+
